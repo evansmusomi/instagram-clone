a94b5f95b176f53486f346e580dc0baf28883497
Evans Musomi <evans.musomi@gmail.com>
2018-04-02 19:41:47 +0300

    adds pic controller actions and views

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,27 @@
 class PicsController < ApplicationController
   def index
   end
+
+  def new
+    @pic = Pic.new
+  end
+
+  def create
+    @pic = Pic.new(pic_params)
+    if @pic.save
+      redirect_to pic_path(@pic)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @pic = Pic.find(params[:id])
+  end
+
+  private
+
+    def pic_params
+      params.require(:pic).permit(:title, :description)
+    end
 end
