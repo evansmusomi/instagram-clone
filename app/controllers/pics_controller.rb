cb242c9c44bc138578f45f35aaa38f54f01b8f01
Evans Musomi <evans.musomi@gmail.com>
2018-04-02 19:51:48 +0300

    updates show pics page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,4 +1,6 @@
 class PicsController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
@@ -9,19 +11,31 @@ class PicsController < ApplicationController
   def create
     @pic = Pic.new(pic_params)
     if @pic.save
-      redirect_to pic_path(@pic)
+      redirect_to @pic, notice: "Yes!!! It was posted."
     else
       render 'new'
     end
   end
 
   def show
-    @pic = Pic.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
   private
 
     def pic_params
       params.require(:pic).permit(:title, :description)
+    end
+
+    def find_pic
+      @pic = Pic.find(params[:id])
     end
 end
